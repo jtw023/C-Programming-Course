e7974c252952be0b637a5b2956cbcd53f8eb1b13
Jordan Walters <R3stock3r@tutanota.com>
2023-07-25 00:29:40 +0100

    Challenge #1 - C program that writes my name

---
+++ challenge1_display_name.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    char first_name[20] = "Jordan";
+    char last_name[20] = "Walters";
+
+    /* printf("Please enter your first name.\n"); */
+    /* scanf("%s", &first_name); */
+    printf("Your first name is: %s\n", first_name);
+
+    /* printf("Please enter your last name.\n"); */
+    /* scanf("%s", &last_name); */
+    printf("Your last name is: %s\n", last_name);
+
+    return 0;
+}
